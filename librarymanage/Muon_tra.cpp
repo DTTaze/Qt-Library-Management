95f24a97c9bf9836ab20a33a351a2d6fdae0d4c5
Le Kien <thatbatngo0108@gmail.com>
2024-10-21 16:36:01 +0700

    chinh lai muontra

--- librarymanage/Muon_tra.cpp
+++ librarymanage/Muon_tra.cpp
@@ -2,35 +2,30 @@
 #include "The_doc_gia.h"
 using namespace std;
 
-/*f. Mượn sách : nhập vào mã thẻ độc giả, chương trình sẽ liệt kê các sách mà độc giả đang mượn. Mỗi độc giả chỉ được mượn tối đa 3 cuốn, 
+/*f. Mượn sách : nhập vào mã thẻ độc giả, chương trình sẽ liệt kê các sách mà độc giả đang mượn. Mỗi độc giả chỉ được mượn tối đa 3 cuốn,
 không cho mượn khi giữ 1 sách quá hạn (7 ngày)
-g. Trả sách 
+g. Trả sách
 h. Liệt kê danh sách các mã sách, tên sách mà 1 độc giả có số thẻ X đang mượn
 i. In danh sách độc giả mượn sách quá hạn theo thứ tự thời gian quá hạn giảm dần
 j. In 10 sách có số lượt mượn nhiều nhất.*/
 
-int TrangThai(Date ngay_muon, Date ngay_tra) { // trạng thái sách của độc giả 
-    int songay = DemSoNgay(ngay_muon, ngay_tra);
-    if(songay != -1) {
-        if(ngay_tra.day == 0) {return 0;} // chưa trả
-        else if(songay<=7) {
-            return 1; // đã trả
-        }
-        else {
-            return 2; // mất sách
-        }
-    } else {cout<<"nhap sai! vui long nhap lai."<<endl;return -1;}
+int TrangThai(Date ngay_muon, Date ngay_tra) { // trạng thái sách của độc giả
+    if(ngay_tra.day == 0) {return 0;} // chưa trả
+    else if(ngay_tra.day >0 ) {
+        return 1; // đã trả
+    }
+
 }
 
 
 int DemSoSachDangMuon(DanhSachMUONTRA *demsach) {
     DanhSachMUONTRA *temp = demsach;
     int dem = 0;
-    
+
     while(temp!=nullptr) {
-        if(temp->data.trangthai == 0 || temp->data.trangthai == 2) {
-        temp = temp->next;
-        dem++;
+        if(temp->data.trangthai == 0) {
+            temp = temp->next;
+            dem++;
         }
     }
     return dem;
@@ -58,32 +53,32 @@ bool Empty(DanhSachMUONTRA * First)
 
 int XoaSachDauTien (DanhSachMUONTRA *&First)
 { DanhSachMUONTRA * p;
-   if (Empty(First))      return 0;
+    if (Empty(First))      return 0;
     p = First;    // nut can xoa la nut dau
     First = p->next;
-    delete p; 
+    delete p;
     return 1;
 }
 
 int XoaSachSauSachP(DanhSachMUONTRA * p)
 {   DanhSachMUONTRA * q;
-  // nếu p là NULL hoặc sau p không có nút
-   if((p == NULL) || (p->next == NULL))	
-      return 0;
-      q = p->next;  // q chi nut can xoa
-      p->next = q->next;
-     delete q;
-     return 1;
+    // nếu p là NULL hoặc sau p không có nút
+    if((p == NULL) || (p->next == NULL))
+        return 0;
+    q = p->next;  // q chi nut can xoa
+    p->next = q->next;
+    delete q;
+    return 1;
 }
 
-int XoaSachTheoThongTin(DanhSachMUONTRA * &head, string ma) {  
+int XoaSachTheoThongTin(DanhSachMUONTRA * &head, string ma) {
     DanhSachMUONTRA * p=head;
-  if (head == NULL ) return 0;
-  if (head->data.masach == ma ) {
+    if (head == NULL ) return 0;
+    if (head->data.masach == ma ) {
         XoaSachDauTien(head); return 1;
-  }
-     
-while (p->next != nullptr) {
+    }
+
+    while (p->next != nullptr) {
         if (p->next->data.masach == ma) {
             XoaSachSauSachP(p);
             return 1;
@@ -121,13 +116,6 @@ while (p->next != nullptr) {
 //     }
 // }
 
-void InManHinhDSSachDangMuon(Danh_Sach_The_Doc_Gia *head) {
-    Danh_Sach_The_Doc_Gia *doc_gia = head;
-    DanhSachMUONTRA *danh_sach = doc_gia->thong_tin.head_lsms;
-    while(danh_sach != nullptr) {
-        cout<<" "<<endl;
-    }
-}
 
 // void LuuNgayMuon(const string& fileName, const Date& ngay_muon) {
 //     ofstream file(fileName);
@@ -140,64 +128,56 @@ void InManHinhDSSachDangMuon(Danh_Sach_The_Doc_Gia *head) {
 //     }
 // }
 
-void ThemSachVaoLSMS(DanhSachMUONTRA * &sach, string ma, const Date &ngayMuon, const Date &ngayTra) {
-    MUONTRA thongtinmuontra (ma, ngayMuon, ngayTra);
-    DanhSachMUONTRA * newLichSu = new DanhSachMUONTRA( thongtinmuontra);
-    if (sach == nullptr)
-    {
-        sach = newLichSu;
-    }
-    else {
-        DanhSachMUONTRA* temp = sach;
-        while(temp->next != nullptr) {
-            temp = temp->next;
-        }
-        temp->next = newLichSu;
-    }
-}
 
-void MuonSach (Danh_Sach_The_Doc_Gia *doc_gia) {
-    Danh_Sach_The_Doc_Gia *tmp = doc_gia;
-    DanhSachMUONTRA *lichsu = tmp->thong_tin.head_lsms;
-    
+void MuonSach (const int& ma_the) {
+    Danh_Sach_The_Doc_Gia *doc_gia = Tim_Kiem(root, ma_the);
+    DanhSachMUONTRA *lichsu = doc_gia->thong_tin.head_lsms;
+
     int sosach = DemSoSachDangMuon(lichsu);
-    if(doc_gia->thong_tin.TrangThai == Khoa || lichsu->data.trangthai == 2 || sosach>3) {
+    if(doc_gia->thong_tin.TrangThai == Khoa || lichsu->data.trangthai == 2 || sosach>=3) {
         cout<<"khong the muon sach"<<endl;
         return;
     } else {
         int luachon = 0; // 1: muon sach    2: thoat ra
-        while (sosach <= 3 || luachon == 2) {
+        while (sosach < 3 || luachon == 1) {
             cout<<"nhap lua chon: "; cin>>luachon;
-            string ma; 
+            string ma;
             cout<<"nhap ma: ";
             getline(cin, ma);
             Date ngaymuon = NgayMuon();
-            Date ngaytra = NgayTraThucTe();
+            Date ngaytra ;
             ThemSach(lichsu, ma, ngaymuon, ngaytra);
-            ThemSachVaoLSMS(lichsu, ma, ngaymuon, ngaytra);
+            sosach++;
+            // ThemSachVaoLSMS(lichsu, ma, ngaymuon, ngaytra);
         }
     }
 }
 
-
-void TraSach (Danh_Sach_The_Doc_Gia *doc_gia, string ma_sach) {
-    Danh_Sach_The_Doc_Gia *temp = doc_gia;
-    
-    while (temp->thong_tin.head_lsms != nullptr) {
-        if(temp->thong_tin.head_lsms->data.masach == ma_sach) {
-            temp->thong_tin.head_lsms->data.NgayTra = NgayTraThucTe();
-            cout<<"da tra sach thanh cong"<<endl;
+DanhSachDauSach *dsds;
+void TraSach (const unsigned int& ma_the, string ma_sach) {
+    Danh_Sach_The_Doc_Gia *doc_gia = Tim_Kiem(root, ma_the);
+    DanhSachMUONTRA *current = doc_gia->thong_tin.head_lsms;
+    string ten_sach = ChuyenMaSachThanhTenSach(*dsds, current->data.masach );
+        if(doc_gia == nullptr || ten_sach == "") {return;}
+    for (int i =0; dsds->node[i] != nullptr; i++ ) {
+        if(current->data.masach == ma_sach) {
+            current->data.NgayTra = NgayTraThucTe();
+            dsds->node[i] = 0;
+            current->data.trangthai = 1;
             return;
-        }else {
-
         }
+        current  = current->next;
     }
-    cout<<"khong co thong tin ve sach doc gia muon" << endl;
 }
 
-void DanhSachSachDocGiaMuon(Danh_Sach_The_Doc_Gia *doc_gia) {
-    cout<<"ten doc gia: " << doc_gia->thong_tin.Ho << " " << doc_gia->thong_tin.Ten << endl;
-    InManHinhDSSachDangMuon(doc_gia);
+void DanhSachSachDocGiaMuon(const unsigned int & ma_the, QTableWidget* tableWidget) {
+    Danh_Sach_The_Doc_Gia *doc_gia = Tim_Kiem(root, ma_the);
+    DanhSachMUONTRA *current = doc_gia->thong_tin.head_lsms;
+    while(current != nullptr) {
+        ChuyenMaSachThanhTenSach(*dsds, current->data.masach );
+        current = current->next;
+        // tableWidget-
+    }
 }
 
 DanhSachMUONTRA* CopyDanhSachMUONTRA(DanhSachMUONTRA *danh_sach_goc) {
