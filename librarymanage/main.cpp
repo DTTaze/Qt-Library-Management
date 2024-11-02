18503f39b77886c372812e9de2a3b50f3e490ab0
DTTaze <90083608+DTTaze@users.noreply.github.com>
2024-11-02 08:02:13 +0700

    c

--- librarymanage/main.cpp
+++ librarymanage/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     LibraryManagementSystem w;
     w.show();
-    w.setFixedSize(1000, 750);  // Đặt kích thước cố định cho cửa sổ (800x600)
+    w.setFixedSize(1200, 750);  // Đặt kích thước cố định cho cửa sổ (800x600)
     return a.exec();
 }
